5618a4687c88d5353ff1e7f9512dbb9741ffe816
AlanZalazar <alan_zz_@hotmail.com>
2025-06-09 17:44:28 -0300

    multiplicar

--- multiplicar.js
+++ multiplicar.js
@@ -1,6 +1,7 @@
-function multiplicar(a, b) {
-    if (isNaN(a) || isNaN(b) || a <= 0 || b <= 0) 
-    return 'Error: Solo se permiten números válidos.';  
-    else{
-    return a * b;}
-    }
+export function multiplicar(a, b) {
+  if (isNaN(a) || isNaN(b) || a <= 0 || b <= 0)
+    return "Error: Solo se permiten números válidos.";
+  else {
+    return a * b;
+  }
+}
